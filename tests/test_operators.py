c9dc03c634bff1090240935863fd0e6979d03932
asoopopo <16204439+Alestainer@users.noreply.github.com>
2024-06-02 11:20:44 +0200

    Add solutions to module 0

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -107,45 +107,50 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    eps = 1e-6
+    assert sigmoid(a) <= 1.0
+    assert sigmoid(a) >= 0.0
+    assert (1 - sigmoid(a)) == sigmoid(-a)
+    assert sigmoid(a) < sigmoid(a + eps)
+    assert sigmoid(0) == 0.5
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c)
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(x: float, y: float) -> None:
     """
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(x, y) == mul(y, x)
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(z: float, x: float, y: float) -> None:
     r"""
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(z, add(x, y)) == add(mul(z, x), mul(z, y))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_floats, small_floats)
+def test_add_commutativity(a: float, b: float) -> None:
     """
-    Write a test that ensures some other property holds for your functions.
+    Write a test that ensures that your addition is commutative i.e.
+    :math:`a + b = b + a`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert a + b == b + a
 
 
 # ## Task 0.3  - Higher-order functions
